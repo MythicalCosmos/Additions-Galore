34f5649fda93fe951cc74126bf5ff0589b84aeab
MythicalCosmos <360mcv@gmail.com>
2024-12-15 21:49:49 -0700

    Base nothing has been added for backup purposes.

---
+++ src/main/java/net/itzstellar/additionsgalore/AdditionsGalore.java
@@ -0,0 +1,15 @@
+package net.itzstellar.additionsgalore;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AdditionsGalore implements ModInitializer {
+	public static final String MOD_ID = "additionsgalore";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/itzstellar/additionsgalore/AdditionsGaloreClient.java
@@ -0,0 +1,10 @@
+package net.itzstellar.additionsgalore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdditionsGaloreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
