4de41c21ed55fb02dc0aadeb2d19a6abea5b09e2
MythicalCosmos <360mcv@gmail.com>
2024-12-16 21:01:05 -0700

    Base nothing has been added for backup purposes.

--- src/main/java/net/itzstellar/additionsgalore/AdditionsGalore.java
+++ src/main/java/net/itzstellar/additionsgalore/AdditionsGalore.java
@@ -2,6 +2,7 @@ package net.itzstellar.additionsgalore;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.itzstellar.additionsgalore.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,5 +12,6 @@ public class AdditionsGalore implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/itzstellar/additionsgalore/item/ModItems.java
@@ -0,0 +1,33 @@
+package net.itzstellar.additionsgalore.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.itzstellar.additionsgalore.AdditionsGalore;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+
+public class ModItems {
+
+    public static final Item PURPLE_SCYTHE = registerItem("purple_scythe", new Item(new Item.Settings()));
+    public static final Item MORGANITE = registerItem("morganite", new Item(new Item.Settings()));
+    public static final Item RAW_MORGANITE = registerItem("raw_morganite", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(AdditionsGalore.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        AdditionsGalore.LOGGER.info("Registering Mod Items for " + AdditionsGalore.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+            entries.add(PURPLE_SCYTHE);
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(MORGANITE);
+            entries.add(RAW_MORGANITE);
+        });
+    }
+}
